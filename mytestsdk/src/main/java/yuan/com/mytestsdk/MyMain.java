7fcb2c85886058d7571cb51bc5a4b496ce659500
Chang <ychang3@MTVL148a3e0b2.local>
2017-05-30 14:57:43 -0700

    init commit

---
+++ mytestsdk/src/main/java/yuan/com/mytestsdk/MyMain.java
@@ -0,0 +1,11 @@
+package yuan.com.mytestsdk;
+
+/**
+ * Created by ychang3 on 5/30/17.
+ */
+
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("hello~MyMain "+args[0]);
+    }
+}
